0aef10bae1d9be86f4febb692f5b6ee6b6edfc6c
Julia Zhuravleva <zhuravlevajuliia@gmail.com>
2024-01-30 02:17:21 +0400

    dice roll returns few parameters

--- dice_roll.py
+++ dice_roll.py
@@ -1,4 +1,4 @@
-# # Example of use: for all necessary skills pass
+# Example of use: for all necessary skills pass
 # skill_data = [
 #     {"player_skill": 8, "required_skill": 70}, # Matching skill 1
 #     {"player_skill": 9, "required_skill": 100}, # Matching skill 2
@@ -44,33 +44,33 @@ def roll_dice(skill_data):
     success_19_20, success_15_18, success_10_14, success_5_9, success_1_4 = calculate_success(skill_data)
     dice_roll = simulate_dice_roll()
 
+    def determine_result_text(success_value):
+        return f"успех на {success_value}%" if success_value >= 0 else f"негативные последствия на {abs(success_value)}%"
+
+    success_values = {
+        19: success_19_20,
+        15: success_15_18,
+        10: success_10_14,
+        5: success_5_9,
+        1: success_1_4
+    }
+
+    success_threshold = next(threshold for threshold in sorted(success_values.keys(), reverse=True) if dice_roll >= threshold)
+    success_value = success_values[success_threshold]
+    success = success_value >= 0
+
     result_text = f"Бросаем кубик, чтобы определить результат...\n"
 
     # Функция для определения текста результата
-    def determine_result_text(success_value):
-        if success_value < 0:
-            return f"негативные последствия на {abs(success_value)}%"
-        else:
-            return f"успех на {success_value}%"
+    for threshold in sorted(success_values.keys(), reverse=True):
+        result_text += f"{threshold}: {determine_result_text(success_values[threshold])}\n"
+    result_text += f"\nВыпало {dice_roll} - это означает {determine_result_text(success_value)}"
 
-    result_text += f"19-20: {determine_result_text(success_19_20)}\n"
-    result_text += f"15-18: {determine_result_text(success_15_18)}\n"
-    result_text += f"10-14: {determine_result_text(success_10_14)}\n"
-    result_text += f"5-9: {determine_result_text(success_5_9)}\n"
-    result_text += f"1-4: {determine_result_text(success_1_4)}\n\n"
-    result_text += f"Выпало {dice_roll} - это означает "
+    return {
+        "result_text": result_text,
+        "dice_roll": dice_roll,
+        "success": success
+    }
 
-    if dice_roll >= 19:
-        result_text += determine_result_text(success_19_20)
-    elif dice_roll >= 15:
-        result_text += determine_result_text(success_15_18)
-    elif dice_roll >= 10:
-        result_text += determine_result_text(success_10_14)
-    elif dice_roll >= 5:
-        result_text += determine_result_text(success_5_9)
-    else:
-        result_text += determine_result_text(success_1_4)
-
-    return result_text
-
-
+# For tests
+# print(roll_dice(skill_data))
